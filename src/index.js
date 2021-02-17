139b303cd1886664c30e72a09edcd2330439cc1c
Natallia Karastsialiova <nats03121998@mail.ru>
2021-02-17 14:31:00 +0300

    feat: write functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(Array.isArray(array) && array.length){
+    var min =array[0];
+    for (let index = 0; index <array.length; index++) {
+       if(array[index] < min){
+            min =array[index];
+       }      
+    }
+    return min;}
+	else{
+		return 0;
+	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) && array.length){
+    var max =array[0];
+    for (let index = 0; index <array.length; index++) {
+       if(array[index] > max){
+            max =array[index];
+       }      
+    }
+    return max;}
+	else{
+		return 0;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array) && array.length){
+    var sum = 0;
+    for (let index = 0; index <array.length; index++) {
+       sum +=array[index]; 
+    }
+    return sum /array.length;}
+	else{
+		return 0;
+	}
 }
